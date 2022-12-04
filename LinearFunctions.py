fbec867ffff3c7bc7738361b80da8fcc3115ad8e
szyszoch <pyrun97531@gmail.com>
2022-12-04 14:54:48 +0100

    Creating window

---
+++ LinearFunctions.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+class Program:
+    def __init__(self):
+        # Creating window
+        self.window = tk.Tk()
+        self.window.title('Linear Functions')
+    def Run(self):
+        self.window.mainloop()
+
+program = Program()
+program.Run()
