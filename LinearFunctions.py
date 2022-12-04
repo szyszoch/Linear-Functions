19dcbe4ab655ee3f05d682279c81bd1c3fe750f3
szyszoch <pyrun97531@gmail.com>
2022-12-04 16:43:04 +0100

    Drawing axes

--- LinearFunctions.py
+++ LinearFunctions.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 
 class Program:
+
     def __init__(self):
+
         # Creating window
         self.window = tk.Tk()
         self.window.title('Linear Functions')
@@ -13,15 +15,36 @@ class Program:
         e1 = tk.Entry(self.LeftPanel); e1.grid(row=0,column=1)
         tk.Button(self.LeftPanel,text='Draw Function',command=self.__DrawFunction()).grid(row=1,column=0,columnspan=2)
 
-        # Creating right panel
+        # Creating canvas
         self.canvas_width = 600
         self.canvas_height = 600
         self.canvas = tk.Canvas(self.window,width=self.canvas_width,height=self.canvas_height, background="white")
         self.canvas.grid(row=0,column=1)
+
+        self.__DrawAxes()
+
     def __DrawAxes(self):
-        # todo
+
+        axes_spacing = 25
+        axes_count = int(self.canvas_width/axes_spacing)
+        main_axes_width = 3
+
+        # X axes
+        for i in range(axes_count):
+            self.canvas.create_line(0,axes_spacing*i,self.canvas_width,axes_spacing*i)
+        # Y axes
+        for i in range(axes_count):
+            self.canvas.create_line(axes_spacing*i,0,axes_spacing*i,self.canvas_height)
+
+        # Main x axis
+        self.canvas.create_line(0,axes_spacing*axes_count/2,self.canvas_width,axes_spacing*axes_count/2,width=main_axes_width)
+
+        # Main y axis
+        self.canvas.create_line(axes_spacing*axes_count/2,0,axes_spacing*axes_count/2,self.canvas_height,width=main_axes_width)
+
+
     def __DrawFunction(self):
-        # todo
+        d= 0
     def Run(self):
         self.window.mainloop()
 
